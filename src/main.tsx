62b671283efa3c69292ed523702ab1aa3e461ace
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-17 17:57:33 +0200

    Set up scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "../cssDist/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <div></div>
+  </React.StrictMode>
 );
