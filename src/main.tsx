20e51f5090e81d665ba5b419a840cddc8f6b2c3c
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-17 18:07:50 +0200

    Implement routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../cssDist/index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div></div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
