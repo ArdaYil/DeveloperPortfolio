2ebe033262a414cbb6c15e69e27c561c0e2b7efe
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-18 20:54:46 +0200

    Implement search bar component

---
+++ src/components/input/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <label className="search-bar">
+      <input className="search-bar__input" type="search" placeholder="Search" />
+    </label>
+  );
+};
+
+export default SearchBar;
