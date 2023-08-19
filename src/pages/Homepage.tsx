71887f6047ff940ae1e37b8e866f36e12aa16950
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-19 11:37:06 +0300

    Implement summer edition section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+import ImageCard from "../components/common/ImageCard";
 import BestSellingProducts from "../components/homepage/BestSellingProducts";
 import Hero from "../components/homepage/Hero";
+import LinkButton from "../components/buttons/LinkButton";
 
 document.title = "Nova Fits | Home";
 
@@ -9,6 +12,15 @@ const Homepage = () => {
       <main>
         <Hero />
         <BestSellingProducts />
+        <ImageCard
+          className="summer-edition"
+          src="/src/assets/images/summer.webp"
+          title="Summer 2023"
+          description="Check out our new summer collection for this year. Now with the best deals ever. Unleash your summer wardrobe!"
+          button={
+            <LinkButton to="/clothing?=summer-editon">Buy Now!</LinkButton>
+          }
+        />
       </main>
     </>
   );
